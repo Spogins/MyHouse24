9e4baf2ddc4214be90f3b738c1fb42df41195d9f
Bogdan <spogins@gmail.com>
2023-01-22 13:49:21 +0200

    account changes

--- account/urls.py
+++ account/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-
-
+    path('login/', LoginView.as_view(), name='login'),
+    path('login_owner/', LoginOwnerView.as_view(), name='login_owner'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+
+class LoginView(View):
+
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, 'account/login.html', context)
+
+    def post(self, request, *args, **kwargs):
+        context = {}
+        return render(request, 'account/login.html', context)
+
+
+class LoginOwnerView(View):
+
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, 'account/login_owner.html', context)
+
+    def post(self, request, *args, **kwargs):
+        context = {}
+        return render(request, 'account/login_owner.html', context)
